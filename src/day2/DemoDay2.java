7b7b96ad364ed0a7b5448b7d9cffbf1ec40ede3b
ajmal <ajmal@localhost>
2023-07-30 19:58:48 -0400

    implemented Add and Subtract method

--- src/day2/DemoDay2.java
+++ src/day2/DemoDay2.java
@@ -8,4 +8,14 @@ public class DemoDay2 {
 		
 		System.out.println ("Test...");
 	}
+	
+	//Add Method ()
+	public static void add (int a, int b) {
+		System.out.println (a+b);
+	}
+	
+	//Subtract Method ()
+	public static void subtract (int a, int b) {
+		System.out.println (a-b);
+	}
 }
